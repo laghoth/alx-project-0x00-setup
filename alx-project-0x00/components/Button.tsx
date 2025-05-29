5fe5c2e042009092b1ce63a484fd0652126d1b5f
OTHMANE LAGHLIMI <laghlim.othmane@gmail.com>
2025-05-29 16:20:23 +0100

    task 6 commit

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
+    
+  return (
+    <button className={`text-white font-medium px-4 py-2 ${styles}`}>
+      {title}
+    </button>
+  );
+};
+
+export default Button;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,17 +1,28 @@
 import React from "react";
 import Card from "@/components/Card";
 import Pill from "@/components/Pill";
+import Button from "@/components/Button";
 
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className="text-xl font-extralight">Landing Page</h1>
       <Card />
-      <p>----------</p>
+      {/* Pill Examples */}
       <div className="p-2 flex gap-2 mt-2">
         <Pill title="Top Villa" />
         <Pill title="Self CheckIn" />
         <Pill title="Free Reschedule" />
+      </div>
+
+      {/* Button Examples */}
+      <div className="mt-6 flex flex-col gap-3">
+        <Button title="Small Button" styles="text-sm rounded-sm bg-blue-500" />
+        <Button
+          title="Medium Button"
+          styles="text-base rounded-md bg-green-100"
+        />
+        <Button title="Large Button" styles="text-lg rounded-full bg-red-500" />
       </div>
     </div>
   );
